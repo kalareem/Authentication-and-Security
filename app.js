fc3fcbdb65b3dc7972816afa41dab8a0ca0fab06
Puripat <black_kalareem@hotmail.com>
2023-03-20 14:34:23 +0700

    Change to mg5 Hashing Password

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const app = express();
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption")
+const md5 = require("md5");
 
 app.set('view engine', 'ejs');
 
@@ -23,7 +23,7 @@ const userSchema = new mongoose.Schema({
 });
 
 // key สำหรับในการ encrypt
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password']});
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = mongoose.model("User", userSchema);
 
@@ -52,7 +52,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   })
   newUser.save(function(err){
     if (err) {
@@ -65,7 +65,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
   User.findOne({email: username}, function(err, foundUser){
     if (err) {
       console.log(err);
